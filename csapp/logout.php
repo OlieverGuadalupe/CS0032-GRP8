7018946c23055c5cc5240a339c7ed49dc8c83e8a
OlieverGuadalupe <jocguadalupe17@gmail.com>
2026-01-13 12:11:56 +0800

    Added logging feature

--- csapp/logout.php
+++ csapp/logout.php
@@ -1,5 +1,14 @@
 <?php
 session_start();
+require_once 'logger.php';
+
+// Log the logout before destroying the session if you want to know who left
+if (isset($_SESSION['logged_in'])) {
+    Logger::info("User logged out", [
+        'ip' => $_SERVER['REMOTE_ADDR']
+    ]);
+}
+
 session_destroy();
 echo json_encode(['success' => true]);
 ?>
